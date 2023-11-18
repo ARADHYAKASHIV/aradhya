ed9a53075bc9426041685ad9f38f9b3414468406
ARADHYAKASHIV <97757514+ARADHYAKASHIV@users.noreply.github.com>
2023-11-18 10:23:47 +0000

    Update nav styling and add messenger animation

--- script.js
+++ script.js
@@ -1,29 +1,93 @@
-// Get the element
-var text = document.getElementById('movingText');
+var Messenger = function(el){
+    'use strict';
+    var m = this;
+    
+    m.init = function(){
+        m.codeletters = "&#*+%?£@§$";
+        m.message = 0;
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        m.messages = [
+            'This is a message, which can be long and all.',
+            'This could be another message.',
+            'Also short ones work!',
+            'Cool.'
+        ];
+        
+        setTimeout(m.animateIn, 100);
+    };
+    
+    m.generateRandomString = function(length){
+        var random_text = '';
+        while(random_text.length < length){
+            random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+        } 
+        
+        return random_text;
+    };
+    
+    m.animateIn = function(){
+        if(m.current_length < m.messages[m.message].length){
+            m.current_length = m.current_length + 2;
+            if(m.current_length > m.messages[m.message].length) {
+                m.current_length = m.messages[m.message].length;
+            }
+            
+            var message = m.generateRandomString(m.current_length);
+            document.getElementById(el).innerHTML = message;
+            
+            setTimeout(m.animateIn, 20);
+        } else { 
+            setTimeout(m.animateFadeBuffer, 20);
+        }
+    };
+    
+    m.animateFadeBuffer = function(){
+        if(m.fadeBuffer === false){
+            m.fadeBuffer = [];
+            for(var i = 0; i < m.messages[m.message].length; i++){
+                m.fadeBuffer.push({c: (Math.floor(Math.random()*12))+1, l: m.messages[m.message].charAt(i)});
+            }
+        }
+        
+        var do_cycles = false;
+        var message = ''; 
+        
+        for(var i = 0; i < m.fadeBuffer.length; i++){
+            var fader = m.fadeBuffer[i];
+            if(fader.c > 0){
+                do_cycles = true;
+                fader.c--;
+                message += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+            } else {
+                message += fader.l;
+            }
+        }
+        
+        document.getElementById(el).innerHTML = message;
+        
+        if(do_cycles === true){
+            setTimeout(m.animateFadeBuffer, 50);
+        } else {
+            setTimeout(m.cycleText, 2000);
+        }
+    };
+    
+    m.cycleText = function(){
+        m.message = m.message + 1;
+        if(m.message >= m.messages.length){
+            m.message = 0;
+        }
+        
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        document.getElementById(el).innerHTML = '';
+        
+        setTimeout(m.animateIn, 200);
+    };
+    
+    m.init();
+}
 
-// Set the initial position
-var position = 0;
-
-// Update the position every 100 milliseconds
-setInterval(function() {
-    // Increase the position by 1
-    position++;
-
-    // Apply the new position to the element
-    text.style.left = position + 'px';
-}, 100);
-
-// Get the Lottie player element
-var player = document.querySelector('dotlottie-player');
-
-// Add a click event listener
-player.addEventListener('click', function() {
-    // Check if the animation is currently playing
-    if (player.isPlaying) {
-        // If it's playing, pause it
-        player.pause();
-    } else {
-        // If it's not playing, play it
-        player.play();
-    }
-});
+console.clear();
+var messenger = new Messenger('#messenger');
